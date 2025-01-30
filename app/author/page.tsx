bba724c81644effbfcd46b8ad642a95bb61685a2
kengitahi <kengitahi@hotmail.com>
2025-01-30 20:52:45 +0300

    Fix

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -1,6 +1,7 @@
 import AuthorInfo from '@/components/organism/authorInfo/AuthorInfo'
 import PostCard from '@/components/molecules/card/PostCard'
 import React from 'react'
+import { getSortedPostsData, PostData } from '@/utils/postsUtil'
 
 export const metadata = {
    title: 'Author Page | MetaBlog',
@@ -8,32 +9,30 @@ export const metadata = {
 }
 
 const Author = () => {
-   return (
-      <main>
-         {/* Author info */}
-         <section>
-            <AuthorInfo />
-         </section>
+   const posts: PostData[] = getSortedPostsData();
 
-         {/* Latest Post */}
-         <section>
-            <div className="container mx-auto mt-12 mb-24 px-5 sm:px-0">
-               <h3 className="text-base-content font-bold text-2xl mb-8">
-                  Latest Post
-               </h3>
-               <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(
-                     (item: any, index: number) => (
-                        <div key={index}>
-                           <PostCard />
-                        </div>
-                     )
-                  )}
-               </div>
-            </div>
-         </section>
-      </main>
-   )
+   return (
+		<main>
+			{/* Author info */}
+			<section>
+				<AuthorInfo />
+			</section>
+
+			{/* Latest Post */}
+			<section>
+				<div className='container mx-auto mt-12 mb-24 px-5 sm:px-0'>
+					<h3 className='text-base-content font-bold text-2xl mb-8'>
+						Latest Post
+					</h3>
+					<div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+						{posts.map((post: any) => (
+							<PostCard key={post.slug} post={post} />
+						))}
+					</div>
+				</div>
+			</section>
+		</main>
+   );
 }
 
 export default Author
